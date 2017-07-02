69588c89c468a40ac2e35ff27b42d2af40d50cc0
rain.xia <rain.xiacong@gmail.com>
2017-07-02 17:35:25 +0800

    vuex

---
+++ components/Counter.vue
@@ -0,0 +1,12 @@
+
+<script>
+import { store } from 'vuex'
+const Counter = {
+    template: `<div>{{ count }}</div>`,
+    computed: {
+        count () {
+            return store.state.count;
+        }
+    }
+}
+</script>
